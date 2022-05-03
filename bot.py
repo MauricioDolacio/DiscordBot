d30a30600e5ac79229e3656a4e41e5c791abe5fa
unknown <mauriciodolacio@outlook.com>
2022-05-02 23:06:27 -0300

    Alterações nas mensagens

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
-import discord
 from discord.ext import commands
 
-bot = commands.Bot("&")
+bot = commands.Bot("&") #Prefixo
 
 @bot.event
 async def on_ready():
-    print("Estou pronto!")
+    print('Estamos logados com {0.user}'.format(bot))
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
+        
     if "bom dia bot" in message.content:
         await message.channel.send(f"Bom dia {message.author.name}")
 
     await bot.process_commands(message)
 
-@bot.command(name="oi") #|oi
+@bot.command(name="oi") #&oi
 async def send_hi(ctx):
     name = ctx.author.name
     response = "Olá, " + name
 
     await ctx.send(response)
 
-bot.run("")
+bot.run("") #Insira o token do bot aqui
