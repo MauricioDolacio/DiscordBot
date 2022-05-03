a39edac1e15b7232253e60bb4fa57e74ccd6eb01
unknown <mauriciodolacio@outlook.com>
2022-05-03 02:17:52 -0300

    Teste dos comandos slash

--- bot.py
+++ bot.py
@@ -1,27 +1,63 @@
+from typing_extensions import Required
 import discord
 from discord.ext import commands
 from discord import guild
 from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_choice, create_option
-
+from discord_slash.utils.manage_commands import create_option, create_choice
+import random
 
 bot = commands.Bot(command_prefix="&") #Prefix
 slash = SlashCommand(bot, sync_commands=True)
-token = "ODc2OTQwMzQ3MTMxOTgxODk1.YRrYVw.4djeMZ5Xjwf9rAOBCbe4f0qY_co" #Insira o token do bot aqui
+token = "" #Insira o token do bot aqui
 
-@slash.slash( #Comandos com /
+
+
+@slash.slash(
     name="hello",
     description="Envia uma mensagem",
+    guild_ids=[610609008373399562], #OBS: Encontrar uma forma de pegar o ID do servidor
+    options=[
+        create_option(
+            name="option",
+            description="Choose your word!",
+            required=True,
+            option_type=3,
+            choices=[
+                create_choice(
+                    name="World!",
+                    value="world"
+                ),
+                create_choice(
+                    name="You!",
+                    value="you"
+                )
+            ]
+        )
+    ]
+)
+
+async def _hello(ctx:SlashContext, option:str):
+    await ctx.send(option)
+
+
+
+@slash.slash(
+    name="roll",
+    description="Rola um d20",
     guild_ids=[610609008373399562]
 )
 
-async def _hello(ctx:SlashContext):
-    await ctx.send("Fala fi")
+async def _roll(ctx: SlashContext):
+    roll = random.randint(1, 20)
+    await ctx.send("Roll:", str(roll))
+
 
 
 @bot.event
 async def on_ready():
     print('Estamos logados com {0.user}'.format(bot))
+
+
 
 @bot.event
 async def on_message(message):
@@ -32,6 +68,8 @@ async def on_message(message):
         await message.channel.send(f"Bom dia {message.author.name}")
 
     await bot.process_commands(message)
+
+
 
 @bot.command(name="oi") #&oi
 async def send_hi(ctx):
